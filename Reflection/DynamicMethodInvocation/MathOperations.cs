4891a6e8ef28460e56d1de826410a7c55d447331
abhay9876 <singhabhayrna987@gmail.com>
2026-02-05 11:20:28 +0530

    [Abhay Singh] Add. MAth Operations

---
+++ Reflection/DynamicMethodInvocation/MathOperations.cs
@@ -0,0 +1,21 @@
+namespace DynamicMethodInvocation
+{
+    public class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+}
