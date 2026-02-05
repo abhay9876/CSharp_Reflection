7b895e7a8c18a9ad000c57033680f737a2fd9eb5
abhay9876 <singhabhayrna987@gmail.com>
2026-02-05 11:25:31 +0530

    [Abhay Singh] Add. Reflection Problems Solved

---
+++ Reflection/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClassInformation;
+using AccessPrivateField;
+using InvokePrivateMethods;
+using DynamicallyCreateObjects;
+using DynamicMethodInvocation;
+using RetriveAttributes;
+using AccessAndModify;
+
+namespace Reflection
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // ClassInfo ci = new ClassInfo();
+            // ci.Display();
+
+            // AccessPrivate ap = new AccessPrivate();
+            // ap.Access();
+
+            // InvokePrivateMethod ip = new InvokePrivateMethod();
+            // ip.InvokeMethod();
+
+            // CreateObjects co = new CreateObjects();
+            // co.CreateObj();
+
+            // MethodInvocation mi = new MethodInvocation();
+            // mi.Invocation();
+
+            // RetriveAttribute ra = new RetriveAttribute();
+            // ra.AuthorInfo();
+
+            // AccessAndModify.Student.ShowCollege();
+            // ModifyStatic.ChangeCollegeName();
+            // AccessAndModify.Student.ShowCollege();
+
+        }
+    }
+}
