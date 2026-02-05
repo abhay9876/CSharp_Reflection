1befa7ca3004e517138d01dce8453210a5574c3f
abhay9876 <singhabhayrna987@gmail.com>
2026-02-05 11:17:00 +0530

    [Abhay Singh] Add. Calculation OPerations

---
+++ Reflection/ClassInformation/Calculation.cs
@@ -0,0 +1,19 @@
+namespace ClassInformation
+{
+    public class Calculation
+    {
+        public int Num1;
+        public int Num2;
+
+        public Calculation(int num1, int num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+
+        public int AddData(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
