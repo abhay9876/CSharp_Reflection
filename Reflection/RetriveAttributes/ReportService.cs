a12a170989f2ad97d4960381da1c42b6a699d885
abhay9876 <singhabhayrna987@gmail.com>
2026-02-05 11:24:36 +0530

    [Abhay Singh] Add. Report about the service

---
+++ Reflection/RetriveAttributes/ReportService.cs
@@ -0,0 +1,12 @@
+namespace RetriveAttributes
+{
+    [Author("Abhay", "1.0")]
+    public class ReportService
+    {
+        public void GenerateReport()
+        {
+            Console.WriteLine("Report Generate Successfully");
+        }
+    }
+
+}
